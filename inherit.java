c5b1741c1b1196c6ac9d8777a0745bdabe5540f8
Yashavanth Gowda B P <yashavanthgowdabp@gmail.com>
2021-11-25 15:14:59 +0530

    ll

---
+++ inherit.java
@@ -0,0 +1,18 @@
+class A
+{
+System.out.println("hello");
+}
+
+class B extends A
+{
+System.out.println("yashavanth");
+}
+
+class Main
+{
+public static void main(string arg[])
+{
+res=new B()
+print(B)
+}
+}
